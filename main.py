2fdfd33dd9075af12e59dcc4ed7dea653809dbe9
Brian Faure <bfaure23@gmail.com>
2017-03-24 21:02:15 -0400

    communication established14

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class grid_worker(QThread):
 		self.job = None 
 
 	def run(self):
-		time_per_cell = 0.1
+		time_per_cell = 0.05
 		if self.job=="bullet":
 			cur_x,cur_y = self.bullet_start
 			increments_per_cell = 8
@@ -174,7 +174,7 @@ class eight_neighbor_grid(QWidget):
 
 				if cell_state==1:
 					qp.setBrush(QColor(self.occupied_color[0],self.occupied_color[1],self.occupied_color[2]))
-					self.current_location = [x,y]
+					#self.current_location = [x,y]
 
 				x_start = x*self.horizontal_step
 				y_start = y*self.vertical_step
@@ -202,10 +202,11 @@ class eight_neighbor_grid(QWidget):
 				else:
 					qp.drawEllipse(render_loc[0]+6,render_loc[1]+(t.bullet_loc[2]*move_m),4,4)
 
-				if self.current_location!=None and self.opponent_location!=None:
-					if t.bullet_loc[0] in [self.current_location[0],self.opponent_location[0]]:
-						if t.bullet_loc[1] in [self.current_location[1],self.opponent_location[1]]:
-							self.game_over = True
+				if self.current_location!=None:
+					if t.bullet_loc[0]==self.current_location[0] and t.bullet_loc[1]==self.current_location[1]:
+						self.game_over = True 
+					if t.bullet_loc[0]==self.opponent_location[0] and t.bullet_loc[1]==self.opponent_location[1]:
+						self.game_over = True 
 
 		if self.opponent_location!=None:
 			x = self.opponent_location[0]
